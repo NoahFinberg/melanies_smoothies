802894610a0c9430bab5cc21164760469b2c0301
Noah Finberg <NoahFinberg@users.noreply.github.com>
2024-06-30 18:22:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,6 @@ st.write("The name on your Smoothie will be", name_on_order)
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
